e8b964305054eb55c2b6d0fe88498c49bfa8a29c
Broken-Admin <brokengaming50@gmail.com>
2018-08-31 00:17:07 -0500

    Add error logging | Change todo + Error logging | index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ console.log("Init message");
 
 //Listener for exceptions
 process.on('uncaughtException', (err) => {
-
-    //TODO error implementation
+    console.log(`Error:\n${err}`)
+    //TODO error processing and evaluation
 });
 
 //Imports
